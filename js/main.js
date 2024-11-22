4f9bd2672809f79a07e49d1ecb558687ebd3a6c5
Allister Mugaisi <allistermugaisi@gmail.com>
2024-11-22 12:29:50 +0300

    feat: added appointment booking page

--- js/main.js
+++ js/main.js
@@ -25,54 +25,128 @@ tabTogglers?.forEach(function (toggler) {
   });
 });
 
-// const popup = new Paystack();
+// Get the modal
+let modal = document.getElementById("book_consultation_modal");
 
-// popup.checkout({
-//   key: "pk_domain_xxxxxx",
-//   email: "allistermugaisi@gmail.com",
-//   amount: 100,
-//   onSuccess: (transaction) => {
-//     console.log(transaction);
-//   },
-//   onLoad: (response) => {
-//     console.log("onLoad: ", response);
-//   },
-//   onCancel: () => {
-//     console.log("onCancel");
-//   },
-//   onError: (error) => {
-//     console.log("Error: ", error.message);
-//   },
-// });
+// Get the button that opens the modal
+let btn = document.getElementById("book_consultation");
 
-const popup = new Paystack();
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
 
-const onElementsMount = (elements) => {
-  if (elements && elements.applePay) {
-    document.querySelector("#pay-button").innerText = "More Payment Options";
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 };
 
-async function payWithPaystack() {
-  try {
-    await popup.paymentRequest({
-      key: "pk_domain_xxxxxx",
-      email: "testuser@paystack.com",
-      amount: 10000,
-      currency: "NGN",
-      container: "payment-request-buttons",
-      loadPaystackCheckoutButton: "pay-button",
-      styles: {
-        theme: "dark",
-        applePay: {
-          width: "100%",
-          height: "50px",
-          borderRadius: "3px",
-          type: "plain",
-          locale: "en",
-        },
+const paymentForm = document.getElementById("paymentForm");
+
+if (paymentForm) {
+  paymentForm.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(this);
+
+    const API_BASE_URL = "https://victoria-visa.onrender.com";
+
+    let payloadForCard = {
+      first_name: formData.get("paystack_first_name"),
+      last_name: formData.get("paystack_last_name"),
+      email: formData.get("paystack_email"),
+      phone: `+254${parseInt(formData.get("paystack_phone"))}`,
+      amount: 200 * 100,
+      metadata: {
+        custom_fields: [
+          {
+            display_name: "Message",
+            variable_name: "message",
+            value: formData.get("paystack_message"),
+          },
+        ],
+        cancel_action: "https://www.victoriavisaconsultants.com",
       },
-      onElementsMount,
-    });
-  } catch (error) {}
+    };
+
+    console.log(payloadForCard);
+
+    const resultElement = document.getElementById("initialize-result");
+    resultElement.innerText = ""; // Clear previous results
+
+    try {
+      const response = await fetch(`${API_BASE_URL}/initialize`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payloadForCard),
+      });
+
+      const result = await response.json();
+      console.log(result);
+      document.getElementById("initialize-result").innerText = JSON.stringify(
+        result,
+        null,
+        2
+      );
+    } catch (error) {
+      document.getElementById(
+        "initialize-result"
+      ).innerText = `Error: ${error.message}`;
+      resultElement.classList.remove("text-green-500"); // Change text color for error
+      resultElement.classList.add("text-red-500");
+    }
+
+    let payloadForMPESA = {
+      first_name: formData.get("paystack_first_name"),
+      last_name: formData.get("paystack_last_name"),
+      email: formData.get("paystack_email"),
+      phone: `+254${parseInt(formData.get("paystack_phone"))}`,
+      amount: 200 * 100,
+      currency: "KES",
+      mobile_money: {
+        phone: `+254${parseInt(formData.get("paystack_phone"))}`,
+        provider: "mpesa",
+      },
+      metadata: {
+        custom_fields: [
+          {
+            display_name: "Message",
+            variable_name: "message",
+            value: formData.get("paystack_message"),
+          },
+        ],
+        cancel_action: "https://www.victoriavisaconsultants.com",
+      },
+    };
+
+    console.log(payloadForMPESA);
+
+    // https://victoria-visa.onrender.com/charge
+
+    // fetch("https://victoria-visa.onrender.com/charge", {
+    //   method: "POST",
+    //   body: formData,
+    // })
+    //   .then(function (response) {
+    //     return response.text();
+    //   })
+    //   .then(function (sessionId) {
+    //     // stripe.redirectToCheckout({ sessionId: sessionId });
+    //   })
+    //   .catch(function (error) {
+    //     console.error(error);
+    //   });
+  });
 }
